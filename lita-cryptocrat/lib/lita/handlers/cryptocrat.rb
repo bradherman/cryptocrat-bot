28e0380c28bf661c9710be4f353ab7118b029722
Brad Herman <brad@hired.com>
2018-01-14 14:08:41 -0800

    formatting

--- lita-cryptocrat/lib/lita/handlers/cryptocrat.rb
+++ lita-cryptocrat/lib/lita/handlers/cryptocrat.rb
@@ -16,6 +16,10 @@ module Lita
         ".price [COIN] [CURRENCY] [exchange]" => "Replies with price on exchange or global average"
       })
 
+      route(/cal [A-Z]{1,5}/, :calendar, command: true, help: {
+        ".cal [COIN]" => "Replies with upcoming events for the coin."
+      })
+
       def price(response)
         args      = response.args
         coin      = args.shift
@@ -27,7 +31,7 @@ module Lita
         url   = "https://min-api.cryptocompare.com/data/price?fsym=#{ coin }&tsyms=#{ currency }&e=#{ exchange }"
         resp  = HTTParty.get(url)
         price = JSON.parse(resp.body)
-        msg   = "#{ coin }: #{ price[currency] }#{ currency }"
+        msg   = "*#{ coin }*: #{ price[currency] }#{ currency }"
 
         if response.message.body.include?('-p')
           response.reply_privately msg
@@ -53,10 +57,10 @@ module Lita
           pct_d     = percent(coin['percent_change_24h'])
           pct_w     = percent(coin['percent_change_7d'])
 
-          msg += "#{ coin['name'] }: #{ coin['symbol'] } - $#{ price_usd } / ฿#{ price_btc }\n"
+          msg += "*#{ coin['name'] }*: #{ coin['symbol'] } - $#{ price_usd } / ฿#{ price_btc }\n"
           msg += "#{ pct_hr }%/hr - #{ pct_d }%/d - #{ pct_w }%/w\n"
-          msg += "Market Cap: $#{ mc_usd }\n"
-          msg += "Supply: #{ available } / #{ max }\n\n"
+          msg += "*Market Cap*: $#{ mc_usd }\n"
+          msg += "*Supply*: #{ available } / #{ max }\n\n"
         end
 
         if response.message.body.include?('-p')
@@ -74,7 +78,7 @@ module Lita
         resp  = HTTParty.get(url)
 
         info  = JSON.parse(resp.body)['DISPLAY'][coin]
-        msg   = "#{ coin }: "
+        msg   = "*#{ coin }*: "
 
         price_usd, price_eth, price_btc = nil, nil, nil
 
@@ -90,7 +94,7 @@ module Lita
         msg += "#{ price_usd } "
         msg += "/ #{ price_eth } " if price_eth
         msg += "/ #{ price_btc } " if price_btc
-        msg += "- MC: #{ cap } - H: #{ high } / L: #{ low } / #{ pct }%"
+        msg += "- *MC*: #{ cap } - *H*: #{ high } / *L*: #{ low } / #{ pct }%"
 
         if response.message.body.include?('-p')
           response.reply_privately msg
